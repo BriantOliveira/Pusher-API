27cbc86766d480f65f893613e4bf5a865c01fd56
Briant Oliveira <briantmoliveira@gmail.com>
2018-11-13 13:58:16 -0800

    Changing user model

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validates :is_signed_in, inclusion: [true, false]
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
